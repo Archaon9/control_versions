25c9980bfb1209bc50f3d95ba6a2be3090cdd7fe
Vic <Vic@ARCHAON>
2017-02-16 16:44:38 +0100

    Agregar archivos de proyecto.

---
+++ Control_de_Versions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_Versions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, suma;
+
+            Console.WriteLine("Entra primer numero");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entra primer numero");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            suma = a + b;
+
+            Console.WriteLine("La suma es: " + suma);
+        }
+    }
+}
